0fa1bfc215b821fc87089c9cd74c2466ae5e6f41
Vasilis Zoumpourlis <v.zuburlis@gmail.com>
2019-09-18 22:02:27 -0500

    slack only the public post

--- load.php
+++ load.php
@@ -1,9 +1,10 @@
 <?php
 
 gila::contentInit( 'post', function(&$table) {
-  // unlist a column from content administration
   $table['events'][] = ['change', function(&$row) {
-    $text = $row['title'].' -> '.gila::base_url('blog/'.$row['slug']);
-    new gpost(gila::option('slack_post_logger.webhook'),['text'=>$text]);
+    if($row['publish']==1) {
+      $text = $row['title'].' -> '.gila::base_url('blog/'.$row['slug']);
+      new gpost(gila::option('slack_post_logger.webhook'), ['text'=>$text]);
+    }
   }];
 });
